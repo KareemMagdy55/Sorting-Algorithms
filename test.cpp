816cc703ec6df3ed146032ba80122b47f3f7cc01
Kareem Magdy <91559979+KareemMagdy55@users.noreply.github.com>
2023-03-19 14:59:19 +0200

    Define each function in "test.cpp"

--- test.cpp
+++ test.cpp
@@ -14,24 +14,48 @@ const int up = 1000; // upper bound of array's elements
 
 template<class T>
 T* fillArr(ll n, ll upperLimit){
+    T* arr = new T[n];
+    for (int i = 0; i < n; ++i) {
+        long double random_double = lb + ( long double)rand() / (( long double)RAND_MAX / (up - lb));
+        arr[i] = random_double ;
+    }
+    return arr;
 }
 
 template<class T>
-
 void test(T arr[], ll n, const function<void(T arr[], ll n)>& sortAlgo){
+    align
+    cout << "Test on array size = " << n << endl;
+    align
 
+    cout << "Array before sort\n";
+    printArr(arr, n);
+    align
+
+    sortAlgo(arr, n);
+    cout << "Array after sort\n";
+    printArr(arr, n)
+
+    align
 
 }
 template<class T>
 void mainTest(ll n, const function<void(T arr[], ll n)>& sortAlgo){
+    T* arr = fillArr<T>(n, up);
+    test(arr, n ,sortAlgo);
+    delete[] arr;
 
 }
 
 template<class T>
 void testAll(const function<void(T arr[], ll n)>& sortAlgo){
+    for (auto size : sizes) {
+        mainTest<T>( size, sortAlgo);
+    }
 
 }
 
 template<class T>
 void testSize(ll n, const function<void(T arr[], ll n)>& sortAlgo){
+    mainTest<T>(n , sortAlgo);
 }
