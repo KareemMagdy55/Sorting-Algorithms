7d54a88fc6dbc39d8670c79abba71c1060a9ca2a
Kareem Magdy <91559979+KareemMagdy55@users.noreply.github.com>
2023-03-19 15:01:13 +0200

    Define functions of sorting algorithms.

--- sort.cpp
+++ sort.cpp
@@ -9,17 +9,50 @@ typedef long long ll;
 template<typename T>
 void selectionSort(T arr[], ll size){
 
+    for (ll i = 0; i < size; ++i) {
+
+        T minEle = arr[i];
+        ll minElePos = i ;
+        for (ll j = i + 1; j < size; ++j) {
+
+            if (arr[j] <= minEle){
+                minElePos = j ;
+                minEle = arr[j];
+            }
+
+        }
+
+        swap(arr[minElePos], arr[i]);
+    }
 }
 
 
 template<typename T>
 void bubbleSort(T arr[], ll size) {
 
+    for (ll i = 0; i < size; ++i) {
+
+        for (int j = 0; j < size - i - 1; ++j) {
+
+            if (arr[j] > arr[j + 1]){
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
 
 
 }
 
 template<typename T>
-void  insertionSort(T arr[], ll size) {
+void  insertionSort(T arr[], ll size){
+    for (ll i = 0; i < size; ++i) {
+        T ele = arr[i];
+        ll j = i - 1;
+        while (ele < arr[j] and j >= 0) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = ele;
+    }
 
 }
